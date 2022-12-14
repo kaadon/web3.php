cf2a1f1fe304dd61dbd189c2d163a1c4850d5edf
“usdtcloud” <usdtcloud@gmail.com>
2022-12-15 03:31:06 +0800

    修改 phpseclib/phpseclib

--- src/Utils.php
+++ src/Utils.php
@@ -15,7 +15,8 @@ use RuntimeException;
 use InvalidArgumentException;
 use stdClass;
 use kornrunner\Keccak;
-use phpseclib\Math\BigInteger as BigNumber;
+use phpseclib3\Math\BigInteger as BigNumber;
+
 
 class Utils
 {
@@ -268,7 +269,7 @@ class Utils
      * 
      * @param BigNumber|string|int $number
      * @param string $unit
-     * @return \phpseclib\Math\BigInteger
+     * @return \phpseclib3\Math\BigInteger
      */
     public static function toWei($number, $unit)
     {
@@ -353,7 +354,7 @@ class Utils
      * 
      * @param BigNumber|string|int $number
      * @param string $unit
-     * @return \phpseclib\Math\BigInteger
+     * @return \phpseclib3\Math\BigInteger
      */
     public static function fromWei($number, $unit)
     {
@@ -474,7 +475,7 @@ class Utils
      * Change number or number string to bignumber.
      * 
      * @param BigNumber|string|int $number
-     * @return array|\phpseclib\Math\BigInteger
+     * @return array|\phpseclib3\Math\BigInteger
      */
     public static function toBn($number)
     {
